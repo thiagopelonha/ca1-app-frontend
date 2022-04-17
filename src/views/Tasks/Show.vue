e638acd8c29dfb13936a435b0dfc257c44013985
thiagopelonha <64002767+thiagopelonha@users.noreply.github.com>
2022-04-17 21:48:03 +0500

    Show Single task screen added

---
+++ src/views/Tasks/Show.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-col>
+    <v-row>
+      <v-btn icon href="/tasks">
+        <v-icon> mdi-chevron-left </v-icon>
+      </v-btn>
+      <h2>Your task</h2>
+    </v-row>
+    <v-row>
+      <v-col>
+        <task-card
+          @onDestroy="onDestroy"
+          :task="task"
+          :editing="false"
+          :show="true"
+        />
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+import { api } from "../../helpers/helpers";
+import TaskCard from "../../components/Tasks/Card.vue";
+
+export default {
+  name: "show",
+  components: {
+    "task-card": TaskCard,
+  },
+  data() {
+    return {
+      task: "",
+    };
+  },
+  methods: {
+    // Methods to emit event from component
+    onDestroy: async function (id) {
+      const sure = window.confirm("Are you sure?");
+      if (!sure) return;
+      await api.deletetask(id);
+      this.$router.push("/tasks");
+    },
+  },
+  async mounted() {
+    this.task = await api.gettask(this.$route.params.id);
+  },
+};
+</script>
